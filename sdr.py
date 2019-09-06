1a0a4869fbbc08cf5449733b2e76a6eb58bf8371
chieh <juliusctw@gmail.com>
2019-09-06 15:58:57 -0400

    added sdr file

---
+++ sdr.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+
+
+
+
+class sdr():
+	def __init__(self, X, Y):
+		self.X = X
+		self.Y = Y
+
+		self.train = 
+
+	def train(self):
+				
+
+if __name__ == "__main__":
+	#print(sys.argv[1], sys.argv[2])
+
+	db = {}
+	fin = open(sys.argv[1],'r')
+	cmds = fin.readlines()
+
